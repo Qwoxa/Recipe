48c9d3eb79747a192b34ab8b5dd78382d67f13df
Nick <nick@rhinodispatch.com>
2019-11-07 11:31:18 -0500

    store

--- frontend/src/store/reducers.js
+++ frontend/src/store/reducers.js
@@ -1,1 +1,50 @@
-import * as types from './types';
+import * as types from "./types";
+
+const initialState = {
+  recipes: [],
+  error: null,
+  loading: false
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case types.CREATE_RECIPE_REQUEST:
+    case types.GET_RECIPES_REQUEST:
+    case types.REMOVE_RECIPE_REQUEST:
+    case types.MODIFY_RECIPE_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+    case types.CREATE_RECIPE_SUCCESS:
+      return {
+        recipes: [...state.recipes, action.payload],
+        loading: false
+      };
+
+    case types.GET_RECIPES_SUCCESS:
+    case types.MODIFY_RECIPE_SUCCESS:
+      return {
+        recipes: action.payload,
+        loading: false
+      };
+
+    case types.REMOVE_RECIPE_SUCCESS:
+      return {
+        recipes: state.recipes.filter(r => r._id !== payload._id),
+        loading: false
+      };
+
+    case types.CREATE_RECIPE_FAILURE:
+    case types.GET_RECIPES_FAILURE:
+    case types.REMOVE_RECIPE_FAILURE:
+    case types.MODIFY_RECIPE_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
+};
--- frontend/src/store/store.test.js
+++ frontend/src/store/store.test.js
@@ -1,36 +1,38 @@
-import configureStore from 'redux-mock-store'
-import moxios from 'moxios';
-import thunk from 'redux-thunk';
-import * as types from './types';
-import * as actions from './actions';
+import configureStore from "redux-mock-store";
+import moxios from "moxios";
+import thunk from "redux-thunk";
+import * as types from "./types";
+import * as actions from "./actions";
 
-const middlewares = [thunk]
-const mockStore = configureStore(middlewares)
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
-const dbresp = [{
-  _id: "5dc4081af04d2941644de949",
-  versions: [
+const dbresp = [
+  {
+    _id: "5dc4081af04d2941644de949",
+    versions: [
       {
-          changed: "2019-11-07T12:03:36.315Z",
-          _id: "5dc4081af04d2941644de94a",
-          name: "Test name",
-          description: "Test desc"
+        changed: "2019-11-07T12:03:36.315Z",
+        _id: "5dc4081af04d2941644de94a",
+        name: "Test name",
+        description: "Test desc"
       }
-  ],
-  createdDate: "2019-11-07T12:03:38.935Z"
-}];
+    ],
+    createdDate: "2019-11-07T12:03:38.935Z"
+  }
+];
 
-describe('store', () => {
-  describe('actions', () => {
+describe("store", () => {
+  describe("actions", () => {
     beforeEach(() => {
-      moxios.install()
+      moxios.install();
     });
-  
+
     afterEach(() => {
-      moxios.uninstall()
+      moxios.uninstall();
     });
-  
-    it('getRecipes', async () => {
+
+    it("getRecipes", async () => {
       moxios.wait(() => {
         const request = moxios.requests.mostRecent();
         request.respondWith({
@@ -38,19 +40,19 @@ describe('store', () => {
           response: dbresp
         });
       });
-  
+
       const expectedActions = [
         { type: types.GET_RECIPES_REQUEST },
         { type: types.GET_RECIPES_SUCCESS, payload: dbresp }
       ];
-      
+
       const store = mockStore({ recipes: [] });
       await store.dispatch(actions.getRecipes());
-  
+
       expect(store.getActions()).toEqual(expectedActions);
     });
 
-    it('createRecipe', async () => {
+    it("createRecipe", async () => {
       moxios.wait(() => {
         const request = moxios.requests.mostRecent();
         request.respondWith({
@@ -58,22 +60,24 @@ describe('store', () => {
           response: dbresp
         });
       });
-  
+
       const expectedActions = [
         { type: types.CREATE_RECIPE_REQUEST },
         { type: types.CREATE_RECIPE_SUCCESS, payload: dbresp }
       ];
-      
+
       const store = mockStore({ recipes: [] });
-      await store.dispatch(actions.createRecipe({
+      await store.dispatch(
+        actions.createRecipe({
           name: "Test name",
           description: "Test desc"
-      }));
-  
+        })
+      );
+
       expect(store.getActions()).toEqual(expectedActions);
     });
 
-    it('removeRecipe', async () => {
+    it("removeRecipe", async () => {
       moxios.wait(() => {
         const request = moxios.requests.mostRecent();
         request.respondWith({
@@ -81,38 +85,40 @@ describe('store', () => {
           response: dbresp
         });
       });
-  
+
       const expectedActions = [
         { type: types.REMOVE_RECIPE_REQUEST },
         { type: types.REMOVE_RECIPE_SUCCESS, payload: dbresp }
       ];
-      
+
       const store = mockStore({ recipes: [] });
-      await store.dispatch(actions.removeRecipe('5dc4081af04d2941644de949'));
-  
+      await store.dispatch(actions.removeRecipe("5dc4081af04d2941644de949"));
+
       expect(store.getActions()).toEqual(expectedActions);
     });
 
-    it('modifyRecipe', async () => {
+    it("modifyRecipe", async () => {
       moxios.wait(() => {
         const request = moxios.requests.mostRecent();
         request.respondWith({
           status: 200,
-          response: 'new state' // TODO
+          response: "new state" // TODO
         });
       });
-  
+
       const expectedActions = [
         { type: types.MODIFY_RECIPE_REQUEST },
-        { type: types.MODIFY_RECIPE_SUCCESS, payload: 'new state' }
+        { type: types.MODIFY_RECIPE_SUCCESS, payload: "new state" }
       ];
-      
+
       const store = mockStore({ recipes: [] });
-      await store.dispatch(actions.modifyRecipe({
-        id: '5dc4081af04d2941644de949',
-        name: 'Test 2'
-      }));
-  
+      await store.dispatch(
+        actions.modifyRecipe({
+          id: "5dc4081af04d2941644de949",
+          name: "Test 2"
+        })
+      );
+
       expect(store.getActions()).toEqual(expectedActions);
     });
   });
